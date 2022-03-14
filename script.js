6b4d8a8a27b070de939614c1d9321f37d3678d56
Benoit Perussel <benoit.14120@hotmail.fr>
2022-03-14 21:33:01 +0100

    fonction lancer dé + ajout score: voir pourquoi cela ne fonctionne pas

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ document.querySelector('.lancer').addEventListener('click', function () {
     if(lanceDe){
         var face = Math.floor(Math.random()* 6)+ 1;
 
-        let resultDe = document.querySelector('')
+        let resultDe = document.querySelector('faces_de');
+        resultDe.src = 'face' + face +'.png';
+
+        if(dice != 1){
+            scoreActuel + dice ;
+            document.getElementById('#scoreActuel' + active).textContent == scoreActuel ;
+        } else {
+            scoreActuel == 0;
+            /* joueur suivant: fonction? variable pour alterner entre les joueurs? */
+        }
 
     }
 })
