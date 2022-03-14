2f12f38882af50ce413d8245e94d158e1f084e97
Benoit Perussel <benoit.14120@hotmail.fr>
2022-03-14 20:55:59 +0100

    ajout des js et condition de lancer du dé

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Déclaration de toutes variable utilisées  var ou let? */
+let scoreTotal;
+let scoreActuel;
+let lanceDe;
+
+/*jet du dé avec l'event click (bouton jet de dé) */
+document.querySelector('.lancer').addEventListener('click', function () {
+    if(lanceDe){
+        var face = Math.floor(Math.random()* 6)+ 1;
+
+        let resultDe = document.querySelector('')
+
+    }
+})
