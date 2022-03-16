320999af310afd4de5ea04fb1fac2d8ba73e60ed
Benoit Perussel <benoit.14120@hotmail.fr>
2022-03-16 18:40:47 +0100

    modif des fonction et correction; resultDe  ne s'accumule pas sur var scoreActuel

--- script.js
+++ script.js
@@ -2,22 +2,23 @@
 var scoreTotal;
 var scoreActuel;
 var lanceDe;
-var tour;
+var tourActif;
 
-document.querySelector('.newgame').addEventListener('click', refresh);
+
+
 /* fonction afin de soit initaliser la partie soit rafraichir le table des joueurs pour une nouvelle partie*/
 function refresh(){
 
     scoreTotal = [0, 0];
     scoreActuel = 0;
-    tour = 0;
+    tourActif = 0;
     lanceDe = true;
 
     document.querySelector('.faces_de').style.display = 'none';
-    document.getElementById('scoreTotal_j1').textContent = '0';
-    document.getElementById('scoreTotal_j2').textContent = '0';
-    document.getElementById('scoreActuel1').textContent = '0';
-    document.getElementById('scoreActuel2').textContent = '0';
+    document.getElementById('scoreTotal_1').textContent = '0';
+    document.getElementById('scoreTotal_2').textContent = '0';
+    document.getElementById('scoreActuel_1').textContent = '0';
+    document.getElementById('scoreActuel_2').textContent = '0';
 
 }
 
@@ -32,8 +33,8 @@ document.querySelector('.lancer').addEventListener('click', function () {
         resultDe.src = 'face_' + face +'.png';
 
         if(face !== 1){
-            scoreActuel += dice ;
-            document.querySelector('#scoreActuel_' + tour).textContent == scoreActuel ;
+            scoreActuel += face ;
+            document.querySelector('#scoreActuel_' + tourActif).textContent == scoreActuel ;
         } else {
 
             joueur_suivant();
@@ -46,17 +47,17 @@ document.querySelector('.lancer').addEventListener('click', function () {
 document.querySelector('.ajout_total').addEventListener('click' , function() {
 
     if(lanceDe){
-        if(tour){
-            scoreTotal += scoreActuel;
-        }
-        
-        document.querySelector('scoreActuel_'+ tour).textContent = scoreTotal[tour];
 
-        if(scoreTotal[tour] >= 100){
-            document.querySelector('#joueur' + tour).textContent = 'Vainqueur';
+            scoreTotal[tourActif] += scoreActuel;
+
+        
+        document.querySelector('#scoreActuel_'+ tourActif).textContent = scoreTotal[tourActif];
+
+        if(scoreTotal[tourActif] >= 100){
+            document.querySelector('#joueur' + tourActif).textContent = 'Vainqueur';
             document.querySelector('.face').style.display = 'none';
             //document.querySelector('.interface_joueur_' + tour).classList.add('vainqueur');
-            document.querySelector('.interface_joueur_' + tour).classList.remove('active');
+            document.querySelector('.interface_joueur_' + tourActif).classList.remove('active');
         }else{
             //joueur suivant
             joueur_suivant();
@@ -67,7 +68,7 @@ document.querySelector('.ajout_total').addEventListener('click' , function() {
 
 function joueur_suivant(){
 
-    tour == 0 ? tour = 1: tour = 0;
+    tourActif === 0 ? tourActif = 1: tourActif = 2;
     scoreActuel = 0;
 
     document.getElementById('scoreActuel_1').textContent = '0';
@@ -78,3 +79,4 @@ function joueur_suivant(){
 }
 
 
+document.querySelector('.newgame').addEventListener('click', refresh);
