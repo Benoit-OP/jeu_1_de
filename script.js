09ed09ee4ba078a04712fe9abc3e4f1cdfa25aaa
Benoit Perussel <benoit.14120@hotmail.fr>
2022-03-16 17:38:55 +0100

    ajout fonction refresh; modif variable et fichiers img renommés; dé n'apparait pas sur la vue (a voir)

--- script.js
+++ script.js
@@ -4,19 +4,39 @@ var scoreActuel;
 var lanceDe;
 var tour;
 
+document.querySelector('.newgame').addEventListener('click', refresh);
+/* fonction afin de soit initaliser la partie soit rafraichir le table des joueurs pour une nouvelle partie*/
+function refresh(){
+
+    scoreTotal = [0, 0];
+    scoreActuel = 0;
+    tour = 0;
+    lanceDe = true;
+
+    document.querySelector('.faces_de').style.display = 'none';
+    document.getElementById('scoreTotal_j1').textContent = '0';
+    document.getElementById('scoreTotal_j2').textContent = '0';
+    document.getElementById('scoreActuel1').textContent = '0';
+    document.getElementById('scoreActuel2').textContent = '0';
+
+}
+
+
 /*jet du dé avec l'event click (bouton jet de dé) */
 document.querySelector('.lancer').addEventListener('click', function () {
     if(lanceDe){
-        var face = Math.floor(Math.random()* 6)+ 1;
+        var face = Math.floor(Math.random() * 6)+ 1;
 
-        let resultDe = document.querySelector('faces_de');
-        resultDe.src = 'face' + face +'.png';
+        var resultDe = document.querySelector('.faces_de');
+        resultDe.style.display = 'block';
+        resultDe.src = 'face_' + face +'.png';
 
         if(face !== 1){
             scoreActuel += dice ;
-            document.getElementById('#scoreActuel' + tour).textContent == scoreActuel ;
+            document.querySelector('#scoreActuel_' + tour).textContent == scoreActuel ;
         } else {
-            scoreActuel == 0;
+
+            joueur_suivant();
             /* joueur suivant: fonction? variable pour alterner entre les joueurs? */
         }
 
@@ -30,7 +50,7 @@ document.querySelector('.ajout_total').addEventListener('click' , function() {
             scoreTotal += scoreActuel;
         }
         
-        document.querySelector('scoreActuel'+ tour).textContent = scoreTotal[tour];
+        document.querySelector('scoreActuel_'+ tour).textContent = scoreTotal[tour];
 
         if(scoreTotal[tour] >= 100){
             document.querySelector('#joueur' + tour).textContent = 'Vainqueur';
@@ -39,9 +59,22 @@ document.querySelector('.ajout_total').addEventListener('click' , function() {
             document.querySelector('.interface_joueur_' + tour).classList.remove('active');
         }else{
             //joueur suivant
+            joueur_suivant();
         }
     }
 
+});
+
+function joueur_suivant(){
+
+    tour == 0 ? tour = 1: tour = 0;
+    scoreActuel = 0;
+
+    document.getElementById('scoreActuel_1').textContent = '0';
+    document.getElementById('scoreActuel_2').textContent = '0';
+    document.querySelector('.interface_joueur_1').classList.toggle('active');
+    document.querySelector('.interface_joueur_2').classList.toggle('active');
+    document.querySelector('.faces_de').style.display = 'none';
+}
 
 
-})
