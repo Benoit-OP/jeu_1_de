01903c8c28b95926573740c6e8d7aa033bf59b7b
Benoit Perussel <benoit.14120@hotmail.fr>
2022-04-05 18:26:40 +0200

    correction erreur NaN au joueur 2

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /* Déclaration de toutes variable utilisées  var ou let? */
 var scoreTotal;
 var scoreActuel;
-var lanceDe;
+
 var tourActif;
+var lanceDe;
+
 
 
 refresh();
+
 /*jet du dé avec l'event click (bouton jet de dé) */
 document.querySelector('.lancer').addEventListener('click', function () {
     if(lanceDe){
@@ -25,22 +28,23 @@ document.querySelector('.lancer').addEventListener('click', function () {
         }
 
     }
-})
+});
 
 document.querySelector('.ajout_total').addEventListener('click' , function() {
-
     if(lanceDe){
 
-            scoreTotal[tourActif] += scoreActuel;
-
+        scoreTotal[tourActif] += scoreActuel;
         
         document.querySelector('#scoreTotal_'+ tourActif).textContent = scoreTotal[tourActif];
 
         if(scoreTotal[tourActif] >= 100){
-            document.querySelector('#joueur_' + tourActif).textContent = 'Vainqueur';
-            document.querySelector('.face').style.display = 'none';
-            //document.querySelector('.interface_joueur_' + tour).classList.add('vainqueur');
+            document.querySelector('#joueur_' + tourActif).textContent = 'VAINQUEUR !';
+            document.querySelector('.faces_de').style.display = 'none';
+            document.querySelector('.interface_joueur_' + tourActif).classList.add('vainqueur');
             document.querySelector('.interface_joueur_' + tourActif).classList.remove('active');
+            
+            //document.querySelector('.interface_joueur_' + tourActif).classList.add('vainqueur');
+            //document.querySelector('.interface_joueur_' + tourActif).classList.remove('active');
             tourActif = false;
         }else{
             //joueur suivant
@@ -52,7 +56,7 @@ document.querySelector('.ajout_total').addEventListener('click' , function() {
 
 function joueur_suivant(){
 
-    tourActif === 1 ? tourActif = 2: tourActif = 1;
+    tourActif === 2 ? tourActif = 1: tourActif = 2;
     scoreActuel = 0;
 
     document.getElementById('scoreActuel_1').textContent = '0';
@@ -62,22 +66,29 @@ function joueur_suivant(){
     document.querySelector('.faces_de').style.display = 'none';
 }
 
-
 document.querySelector('.newgame').addEventListener('click', refresh);
-
 
 /* fonction afin de soit initaliser la partie soit rafraichir le table des joueurs pour une nouvelle partie*/
 function refresh(){
 
-    scoreTotal = [0, 0];
+    scoreTotal = [0, 0 , 0];
     scoreActuel = 0;
-    tourActif = 0;
+    tourActif = 1;
     lanceDe = true;
+
 
     document.querySelector('.faces_de').style.display = 'none';
     document.getElementById('scoreTotal_1').textContent = '0';
     document.getElementById('scoreTotal_2').textContent = '0';
     document.getElementById('scoreActuel_1').textContent = '0';
     document.getElementById('scoreActuel_2').textContent = '0';
+    document.getElementById('joueur_1').textContent = 'JOUEUR 1';
+    document.getElementById('joueur_2').textContent = 'JOUEUR 2';
+    document.querySelector('.interface_joueur_1').classList.remove('vainqueur');
+    document.querySelector('.interface_joueur_2').classList.remove('vainqueur');
+    document.querySelector('.interface_joueur_1').classList.remove('active');
+    document.querySelector('.interface_joueur_2').classList.remove('active');
+
+    document.querySelector('.interface_joueur_1').classList.add('active');
 
 }
